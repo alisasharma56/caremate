81d9c590c421153f443d8971e6088ab66cc3b64c
Sanjay <sanjay.pahari098@gmail.com>
2026-07-13 16:25:37 +0545

    refactor(route): add routes for feed

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -1,10 +1,11 @@
 import { Outlet } from '@tanstack/react-router'
-import { Sidebar } from './sidebar'
+import { Sidebar } from '@/components/sidebar'
+
 export function RootLayout() {
   return (
     <main style={{ display: 'flex', minHeight: '100vh' }}>
       <Sidebar />
-        <section style={{ flex: 1, minWidth: 0, padding: '32px' }}>
+      <section style={{ flex: 1, minWidth: 0 }}>
         <Outlet />
       </section>
     </main>
