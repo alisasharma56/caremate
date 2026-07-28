c69d1c515795281de33678289b3e3449f4a417b7
Alisa <alisasharma23@gmail.com>
2026-07-28 12:07:56 +0545

    feat(component):Added onboarding features

--- src/features/SidecardPanel/SideCardPanel.css.ts
+++ src/features/SidecardPanel/SideCardPanel.css.ts
@@ -1,15 +1,3 @@
-// import { style } from "@vanilla-extract/css";
-// import { space } from "@/styles/theme/tokens.css";
-//
-// export const stack = style({
-//     display: "flex",
-//     flexDirection: "column",
-//     gap: space[4],
-//     backgroundColor:"White"
-// });
-
-
-
 import { style } from "@vanilla-extract/css";
 import { space } from "@/styles/theme/tokens.css";
 
